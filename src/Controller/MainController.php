b9e4b06246d3a3d52481c4971d51afb612c548f5
Valentin Pradel <val.pxl.vp@gmail.com>
2023-07-11 15:20:20 +0200

    Améliorer la strutcure de twig avec l'héritage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,18 +7,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-//    #[Route("/", name: "main_home")]
-
-    /**
-     * @Route("/", name="main_home")
-     */
+    #[Route("/", name: "main_home")]
     public function home() {
         return $this->render('main/home.html.twig');
     }
 
-    /**
-     * @Route("/test", name="main_test")
-     */
+    #[Route("/test", name: "main_test")]
     public function test() {
         return $this->render('main/test.html.twig');
     }
